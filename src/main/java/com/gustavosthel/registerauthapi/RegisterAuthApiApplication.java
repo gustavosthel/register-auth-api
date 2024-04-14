09775b77f8226b84396224fff807ef5a6c0b2d09
Gustavo Sthel <gustavosthel@gmail.com>
2024-04-14 08:46:47 -0300

    First commit

---
+++ src/main/java/com/gustavosthel/registerauthapi/RegisterAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.gustavosthel.registerauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterAuthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavosthel/registerauthapi/RegisterAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavosthel.registerauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
